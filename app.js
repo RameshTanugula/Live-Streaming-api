73a6c870c825e2806c531e32aad09588db26e47e
RameshTanugula <gnitramesh@gmail.com>
2022-11-27 21:51:28 +0530

    final changes
    final changes

--- app.js
+++ app.js
@@ -2,15 +2,32 @@ const sql = require("./db/db");
 // Requiring the module
 const cors = require("cors");
 const express = require('express');
+var bodyParser = require('body-parser');
 const bcrypt = require('bcryptjs');
+var nodemailer = require('nodemailer');
 const saltRounds = 10;
 const app = express();
-app.use(express.json());
+// app.use(express.json());
 var corsOptions = {
   origin: "*"
 };
 app.use(cors(corsOptions));
+app.use(bodyParser.json({limit: '50mb'}));
+app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
+/**
+ * email
+ */
+ var transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: 'homeliveevents@gmail.com',
+    pass: 'jqwywxvjkvcopgor'
+  }
+});
 // Handling '/' request
+app.get('/', (req, res) => {
+  res.send('Live streaming API is working!')
+});
 app.post('/register', (req, res) => {
   const data = req.body;
   bcrypt.hash(data.password, saltRounds, function (err, hash) {
@@ -40,24 +57,47 @@ app.post('/checkout', (req, res) => {
         if (dataErr) {
           res.send({ error: `Something went wrong ${dataErr}` });
         } else {
-          res.send({ res: 'Data Saved Successfully' });
+          // res.send({ res: 'Data Saved Successfully' });
+          const userData = data.userData;
+          console.log(userData, 'userdata***')
+          var mailOptions = {
+            from: 'homeliveevents@gmail.com',
+            to: ['homeliveevents@gmail.com', 'gnitramesh@gmail.com'],
+            subject: `Event registered on ${data?.event_date}`,
+            html: `<h4><b>Dear Admin</b></h4><p> New event booked at  ${data?.event_date} </p>
+            <br />
+            User Name: ${userData.name} <br/>
+            User Email: ${userData.email} <br/>
+            User Mobile: ${userData.mobile} <br/>
+            <a href="${data.domain}"> ${data.domain} </a>`
+          };
+          transporter.sendMail(mailOptions, function(error, info){
+            if (error) {
+              res.send('Email not sent to admin');
+            } else {
+              console.log('Email sent: ' + info.response);
+              res.send('Email sent to Admin');
+            }
+          });
         }
       });
 });
 // login
 app.post('/login', (req, res) => {
-  const mobile = req.body.mobile;
+  const userId = req.body.mobile;
   const password = req.body.password;
-  if (mobile && password) {
+  if (userId && password) {
 
-    let query = `select * from users where mobile ='${mobile}'`;
+    let query = `select * from users where email ='${userId}'`;
     sql.query(query, (err, result) => {
       if (result?.length > 0) {
-        bcrypt.compare(password, result[0].password, function (err, result) {
-          if (err || !result) {
+        bcrypt.compare(password, result[0].password, function (err, passResult) {
+          if (err || !passResult) {
+            res.status(400)
             res.send({ error: 'Login Failed' });
           } else {
-            res.send({ message: 'Login success' });
+            res.status(200);
+            res.send({ message: 'Login success', data:result[0] });
           }
         });
       } else {
@@ -68,6 +108,57 @@ app.post('/login', (req, res) => {
     res.send({ message: 'Login falied' })
   }
 });
+app.get('/get/users', (req, res) => {
+    let query = `select * from users`;
+    sql.query(query, (err, result) => {
+       if (err || !result) {
+            res.send({ message: 'No users data found' });
+          } else {
+            res.send({ data:result });
+          }
+        });
+     });
+    
+     app.post('/update/videoid', function (req,res) {
+      var query = `update user_images set video_id='${req.body.videoId}' where user_id = '${req.body.userId}'`;
+      
+      sql.query(query, (err, result) => {
+        if (err) {
+          res.send({ message: 'Data not saved' });
+        } else {
+          res.send({ message: 'Data Saved' });
+        }
+      });
+      
+      });
+
+app.post('/upload/images', function (req,res) {
+var query = '';
+if(req.body.type==='banner'){
+query = "delete from user_images where type='banner'; INSERT INTO `user_images`(`user_id`, `img`, `type`) VALUES ('" + req.body.user_id + "','" + req.body.img + "', '" + req.body.type + "')";
+} else {
+  query = "INSERT INTO `user_images`(`user_id`, `img`, `type`) VALUES ('" + req.body.user_id + "','" + req.body.img + "', '" + req.body.type + "')";
+  }
+sql.query(query, (err, result) => {  
+  if (err) {
+    res.send({ message: 'Data not saved' });
+  } else {
+    res.send({ message: 'Data Saved' });
+  }
+});
+
+});
+app.get('/get/images/:id', function (req,res) {
+  let query = `select * from user_images where user_id='${req.params.id}'`;
+  sql.query(query, (err, result) => {
+     if (err || !result) {
+          res.send({ message: 'No users data found' });
+        } else {
+          res.send({ data:result });
+        }
+      });
+   
+  });
 app.listen(3000, () => {
   console.log('server listening on port 3000');
 });
--- config/db_table.sql
+++ config/db_table.sql
@@ -16,5 +16,17 @@ create table users(
     event_date VARCHAR(100) NOT NULL,
     promo_code VARCHAR(100) NOT NULL,
     user_id VARCHAR(200) NOT NULL,
+    cost VARCHAR(10) NOT NULL,
+    event_type VARCHAR(100) NOT NULL,
+    user_details TEXT NOT NULL,
+    PRIMARY KEY ( id )
+ );
+
+ create table user_images(
+    id INT NOT NULL AUTO_INCREMENT,
+    user_id VARCHAR(50) NOT NULL,
+    video_id VARCHAR(100) NOT NULL,
+    type VARCHAR(50) NOT NULL,
+    img longtext NOT NULL,
     PRIMARY KEY ( id )
  );
